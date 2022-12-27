124406efde366271cfdb9b406545d697a809f56c
ihor-palii <palii.ihor@uvik.net>
2022-12-27 22:42:02 +0200

    some components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
-import { faBookOpen, faBuildingColumns, faCheck, faCoins, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import {Component} from '@angular/core';
+import {faBookOpen, faBuildingColumns, faCheck, faCoins, faPeopleGroup} from '@fortawesome/free-solid-svg-icons';
+
+enum Steps {
+  Information,
+  HouseholdBenefits,
+  GovPrograms,
+  PersonalIncome,
+  Result,
+}
+
+enum StepState {
+  Disabled,
+  Skipped,
+  Active,
+}
 
 @Component({
   selector: 'app-root',
@@ -7,6 +21,42 @@ import { faBookOpen, faBuildingColumns, faCheck, faCoins, faPeopleGroup } from '
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gnp-ca-screener';
-  icons = {faBookOpen, faBuildingColumns, faCheck, faCoins, faPeopleGroup }
+  // enums redeclaration to be available in template
+  Steps = Steps;
+  StepState = StepState;
+
+  // component variables
+  icons = {faBookOpen, faBuildingColumns, faCheck, faCoins, faPeopleGroup};
+  currentStep = Steps.Information;
+  steps = {
+    household: StepState.Disabled,
+    programs: StepState.Disabled,
+    income: StepState.Disabled,
+    result: StepState.Disabled,
+  }
+  fields = {
+    householdMembersCount: 1,
+    householdMembersBenefit: false,
+  }
+
+  goToStep(step: Steps) {
+    this.currentStep = step;
+  }
+
+  goToHouseholdsStep() {
+    this.currentStep = Steps.HouseholdBenefits;
+    this.steps.household = StepState.Active;
+  }
+
+  goToProgramsStep() {
+    if (this.fields.householdMembersBenefit) {
+      this.currentStep = Steps.Result
+      this.steps.programs = StepState.Skipped;
+      this.steps.income = StepState.Skipped;
+      this.steps.result = StepState.Active;
+    } else {
+      this.currentStep = Steps.GovPrograms;
+      this.steps.programs = StepState.Active;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTabsModule} from "@angular/material/tabs";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {ButtonComponent} from './components/button/button.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatRadioModule} from "@angular/material/radio";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatTabsModule,
-        FontAwesomeModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    FontAwesomeModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    MatRadioModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
